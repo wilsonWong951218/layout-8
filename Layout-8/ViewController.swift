c58c1dfd7cde08e317574ec75c7e2126bdcc5bf2
wilson <wong_wei_wen@hotmail.com>
2018-05-05 21:04:51 +0800

    Initial Commit

---
+++ Layout-8/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Layout-8
+//
+//  Created by Macintosh on 2018/5/5.
+//  Copyright © 2018年 LAT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
